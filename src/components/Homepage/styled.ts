9d44f90bcc6a18bc875d4efe6ed7a786b29530ba
feh-franc0 <fernandofv1110@gmail.com>
2023-09-19 15:52:27 -0300

    Home Page concluida

--- src/components/Homepage/styled.ts
+++ src/components/Homepage/styled.ts
@@ -1,15 +1,16 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  padding-top: 50px;
   width: 100%;
   text-align: center;
   background-color: #ffff;
+  height: 100%;
 `
 export const Content = styled.div`
   display: flex;
   justify-content: space-between;
   max-width: 1280px;
-  background-color: blue;
   margin: auto;
   &:hover{
     /* cursor: pointer; */
@@ -18,24 +19,33 @@ export const Content = styled.div`
 export const Leftdiv = styled.div`
   display: flex;
   flex-direction: column;
+  max-width: 500px;
   align-items: start;
-  max-width: 300px;
-  background-color: red;
+  justify-content: space-around;
+  margin: 20px 0;
+
   h1{
     text-align: start;
+    color: #032D60;
+    font-size: 2.7rem;
   }
   p{
     text-align: start;
+    font-size: 1.3rem;
+    color: #2d2d2d;
   }  
   div{
     display: flex;
+    position: relative;
+    gap: 50px;
+
     button{
       border: none;
       border-radius: 10px;
       height: 50px;
       color: white;
       margin: auto;
-      background-color: #137d00;
+      background-color: #0176D3;
       font-family: Arial, Helvetica, sans-serif;
       font-weight: bold;
       padding: 5px 15px;
@@ -46,7 +56,6 @@ export const Rightdiv = styled.div`
   align-items: end;
   position: relative;
   img{
-    width: 400px;
     height: 400px;
   }
   
