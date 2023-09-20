8cd4c4a587d44cd13644fd70622a77a947ac258f
felipepx <feliperaxz@gmail.com>
2023-09-20 18:24:07 -0300

    estrutura do componente Title finalizada

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,20 +1,19 @@
-import React from 'react'
 import { Container } from './styled';
 import Button from '../Button';
 
 
 interface IProps{
   title: String;
-  description: String;
-  textButton: String;
-  typeButton: "withBorder"|"withBg"
+  description?: String;
+  textButton?: String;
+  typeButton?: "withBorder"|"withBg"
 }
-function Title({title,description,textButton,typeButton}: IProps) {
+function Title({title,description,textButton,typeButton='withBg'}: IProps) {
   return (
     <Container>
       <h1>{title}</h1>
-      <p>{description}</p>
-      <Button textButton={textButton} typeButton={typeButton}/>
+      {description && <p>{description}</p>}
+      {textButton && <Button textButton={textButton} typeButton={typeButton}/>}
     </Container>
   )
 }
