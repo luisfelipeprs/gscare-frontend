214dfb350d6c99353e2f2f205adb0a0d1b30eb92
feh-franc0 <fernandofv1110@gmail.com>
2023-10-03 19:13:46 -0300

    react-router-dom instalado : falta configurar cliques e botoes referentes as rotas

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+// import App from './App.tsx'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from './pages/Home/index.tsx';
+import FormLeads from './pages/FormLeads/index.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/form",
+    element: <FormLeads />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </React.StrictMode>,
 )
---
+++ src/pages/FormLeads/index.tsx
@@ -0,0 +1,9 @@
+
+function FormLeads() {
+  return (
+      <>
+        <h1>Pagina formlead</h1>
+      </>
+    )
+}
+export default FormLeads;
