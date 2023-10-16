d7c44bb2921efc8227e3bbd50940325f2e1afad9
felipepx <feliperaxz@gmail.com>
2023-10-16 10:10:33 -0300

    floatingmenu finished

--- src/components/FloatingMenu/styled.ts
+++ src/components/FloatingMenu/styled.ts
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 
 export const StyledFloatingMenu = styled.div`
+  display: flex;
+  align-items: center;
   position: relative;
-  padding: 25px;
+  padding: 5px;
   width: 100%;
   height: 100%;
   margin: 10px;
@@ -11,3 +13,37 @@ export const StyledFloatingMenu = styled.div`
   border: 1px solid #666;
   border-radius: 5px;
 `;
+
+export const Img = styled.img`
+  width: 60px;
+  border: 1px solid black;
+  border-radius: 40px;
+  margin: 10px 10px;
+  margin-left: 10px;
+`
+export const Infos = styled.div`
+  display: flex;
+  height: 100%;
+  flex-wrap: wrap;
+  flex-direction: column;
+  
+  p{
+    display: flex;
+    
+    margin: 5px;
+    flex: 2;
+    
+  }
+`
+
+export const SignOutInfo = styled.div`
+  margin: 20px;
+`
+export const Container = styled.div`
+width: 100%;
+display: flex;
+justify-content: space-between;
+align-items: center;
+`
+
+
