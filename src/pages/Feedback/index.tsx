d1a4e505f2c4f3881f541357386f4d3239f33592
felipepx <feliperaxz@gmail.com>
2023-11-29 12:06:09 -0300

    adding date and time to card feedback

--- src/pages/Feedback/index.tsx
+++ src/pages/Feedback/index.tsx
@@ -32,54 +32,10 @@ function Feedback() {
                 <Strong>Typography</Strong> is the art and technique of arranging type to
                 make written language legible, readable and appealing when displayed.
               </Text>
-            </Card>
-
-            <Card style={{ maxWidth: 240 }}>
-              <Flex gap="3" align="center">
-                <Avatar
-                  size="3"
-                  src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
-                  radius="full"
-                  fallback="T"
-                />
-                <Box>
-                  <Text as="div" size="2" weight="bold">
-                    Teodros Girmay
-                  </Text>
-                  <Text as="div" size="2" color="gray">
-                    Engineering
-                  </Text>
-                </Box>
-              </Flex>
               <br />
-              <Text as="p" size="3">
-                <Strong>Typography</Strong> is the art and technique of arranging type to
-                make written language legible, readable and appealing when displayed.
-              </Text>
-            </Card>
-
-            <Card style={{ maxWidth: 240 }}>
-              <Flex gap="3" align="center">
-                <Avatar
-                  size="3"
-                  src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
-                  radius="full"
-                  fallback="T"
-                />
-                <Box>
-                  <Text as="div" size="2" weight="bold">
-                    Teodros Girmay
+              <Text as="div" size="1" color="gray">
+                    11/09/2001 as 22:00
                   </Text>
-                  <Text as="div" size="2" color="gray">
-                    Engineering
-                  </Text>
-                </Box>
-              </Flex>
-              <br />
-              <Text as="p" size="3">
-                <Strong>Typography</Strong> is the art and technique of arranging type to
-                make written language legible, readable and appealing when displayed.
-              </Text>
             </Card>
             <Card style={{ maxWidth: 240 }}>
               <Flex gap="3" align="center">
@@ -103,8 +59,11 @@ function Feedback() {
                 <Strong>Typography</Strong> is the art and technique of arranging type to
                 make written language legible, readable and appealing when displayed.
               </Text>
-            </Card>
-            <Card style={{ maxWidth: 240 }}>
+              <br />
+              <Text as="div" size="1" color="gray">
+                    11/09/2001 as 22:00
+                  </Text>
+            </Card>            <Card style={{ maxWidth: 240 }}>
               <Flex gap="3" align="center">
                 <Avatar
                   size="3"
@@ -126,8 +85,11 @@ function Feedback() {
                 <Strong>Typography</Strong> is the art and technique of arranging type to
                 make written language legible, readable and appealing when displayed.
               </Text>
-            </Card>
-            <Card style={{ maxWidth: 240 }}>
+              <br />
+              <Text as="div" size="1" color="gray">
+                    11/09/2001 as 22:00
+                  </Text>
+            </Card>            <Card style={{ maxWidth: 240 }}>
               <Flex gap="3" align="center">
                 <Avatar
                   size="3"
@@ -149,8 +111,11 @@ function Feedback() {
                 <Strong>Typography</Strong> is the art and technique of arranging type to
                 make written language legible, readable and appealing when displayed.
               </Text>
-              </Card>
-            <Card style={{ maxWidth: 240 }}>
+              <br />
+              <Text as="div" size="1" color="gray">
+                    11/09/2001 as 22:00
+                  </Text>
+            </Card>            <Card style={{ maxWidth: 240 }}>
               <Flex gap="3" align="center">
                 <Avatar
                   size="3"
@@ -172,8 +137,11 @@ function Feedback() {
                 <Strong>Typography</Strong> is the art and technique of arranging type to
                 make written language legible, readable and appealing when displayed.
               </Text>
-            </Card>
-            <Card style={{ maxWidth: 240 }}>
+              <br />
+              <Text as="div" size="1" color="gray">
+                    11/09/2001 as 22:00
+                  </Text>
+            </Card>            <Card style={{ maxWidth: 240 }}>
               <Flex gap="3" align="center">
                 <Avatar
                   size="3"
@@ -195,8 +163,11 @@ function Feedback() {
                 <Strong>Typography</Strong> is the art and technique of arranging type to
                 make written language legible, readable and appealing when displayed.
               </Text>
-            </Card>
-            <Card style={{ maxWidth: 240 }}>
+              <br />
+              <Text as="div" size="1" color="gray">
+                    11/09/2001 as 22:00
+                  </Text>
+            </Card>            <Card style={{ maxWidth: 240 }}>
               <Flex gap="3" align="center">
                 <Avatar
                   size="3"
@@ -218,8 +189,11 @@ function Feedback() {
                 <Strong>Typography</Strong> is the art and technique of arranging type to
                 make written language legible, readable and appealing when displayed.
               </Text>
-            </Card>
-            <Card style={{ maxWidth: 240 }}>
+              <br />
+              <Text as="div" size="1" color="gray">
+                    11/09/2001 as 22:00
+                  </Text>
+            </Card>            <Card style={{ maxWidth: 240 }}>
               <Flex gap="3" align="center">
                 <Avatar
                   size="3"
@@ -241,164 +215,10 @@ function Feedback() {
                 <Strong>Typography</Strong> is the art and technique of arranging type to
                 make written language legible, readable and appealing when displayed.
               </Text>
-            </Card>
-            <Card style={{ maxWidth: 240 }}>
-              <Flex gap="3" align="center">
-                <Avatar
-                  size="3"
-                  src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
-                  radius="full"
-                  fallback="T"
-                />
-                <Box>
-                  <Text as="div" size="2" weight="bold">
-                    Teodros Girmay
-                  </Text>
-                  <Text as="div" size="2" color="gray">
-                    Engineering
-                  </Text>
-                </Box>
-              </Flex>
               <br />
-              <Text as="p" size="3">
-                <Strong>Typography</Strong> is the art and technique of arranging type to
-                make written language legible, readable and appealing when displayed.
-              </Text>
-            </Card><Card style={{ maxWidth: 240 }}>
-              <Flex gap="3" align="center">
-                <Avatar
-                  size="3"
-                  src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
-                  radius="full"
-                  fallback="T"
-                />
-                <Box>
-                  <Text as="div" size="2" weight="bold">
-                    Teodros Girmay
+              <Text as="div" size="1" color="gray">
+                    11/09/2001 as 22:00
                   </Text>
-                  <Text as="div" size="2" color="gray">
-                    Engineering
-                  </Text>
-                </Box>
-              </Flex>
-              <br />
-              <Text as="p" size="3">
-                <Strong>Typography</Strong> is the art and technique of arranging type to
-                make written language legible, readable and appealing when displayed.
-              </Text>
-            </Card>
-            <Card style={{ maxWidth: 240 }}>
-              <Flex gap="3" align="center">
-                <Avatar
-                  size="3"
-                  src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
-                  radius="full"
-                  fallback="T"
-                />
-                <Box>
-                  <Text as="div" size="2" weight="bold">
-                    Teodros Girmay
-                  </Text>
-                  <Text as="div" size="2" color="gray">
-                    Engineering
-                  </Text>
-                </Box>
-              </Flex>
-              <br />
-              <Text as="p" size="3">
-                <Strong>Typography</Strong> is the art and technique of arranging type to
-                make written language legible, readable and appealing when displayed.
-              </Text>
-            </Card><Card style={{ maxWidth: 240 }}>
-              <Flex gap="3" align="center">
-                <Avatar
-                  size="3"
-                  src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
-                  radius="full"
-                  fallback="T"
-                />
-                <Box>
-                  <Text as="div" size="2" weight="bold">
-                    Teodros Girmay
-                  </Text>
-                  <Text as="div" size="2" color="gray">
-                    Engineering
-                  </Text>
-                </Box>
-              </Flex>
-              <br />
-              <Text as="p" size="3">
-                <Strong>Typography</Strong> is the art and technique of arranging type to
-                make written language legible, readable and appealing when displayed.
-              </Text>
-            </Card>
-            <Card style={{ maxWidth: 240 }}>
-              <Flex gap="3" align="center">
-                <Avatar
-                  size="3"
-                  src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
-                  radius="full"
-                  fallback="T"
-                />
-                <Box>
-                  <Text as="div" size="2" weight="bold">
-                    Teodros Girmay
-                  </Text>
-                  <Text as="div" size="2" color="gray">
-                    Engineering
-                  </Text>
-                </Box>
-              </Flex>
-              <br />
-              <Text as="p" size="3">
-                <Strong>Typography</Strong> is the art and technique of arranging type to
-                make written language legible, readable and appealing when displayed.
-              </Text>
-            </Card><Card style={{ maxWidth: 240 }}>
-              <Flex gap="3" align="center">
-                <Avatar
-                  size="3"
-                  src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
-                  radius="full"
-                  fallback="T"
-                />
-                <Box>
-                  <Text as="div" size="2" weight="bold">
-                    Teodros Girmay
-                  </Text>
-                  <Text as="div" size="2" color="gray">
-                    Engineering
-                  </Text>
-                </Box>
-              </Flex>
-              <br />
-              <Text as="p" size="3">
-                <Strong>Typography</Strong> is the art and technique of arranging type to
-                make written language legible, readable and appealing when displayed.
-              </Text>
-            </Card>
-            <Card style={{ maxWidth: 240 }}>
-              <Flex gap="3" align="center">
-                <Avatar
-                  size="3"
-                  src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
-                  radius="full"
-                  fallback="T"
-                />
-                <Box>
-                  <Text as="div" size="2" weight="bold">
-                    Teodros Girmay
-                  </Text>
-                  <Text as="div" size="2" color="gray">
-                    Engineering
-                  </Text>
-                </Box>
-              </Flex>
-              <br />
-              <Text as="p" size="3">
-                <Strong>Typography</Strong> is the art and technique of arranging type to
-                make written language legible, readable and appealing when displayed.
-              </Text>
             </Card>
           </Flex>
         </Theme>
