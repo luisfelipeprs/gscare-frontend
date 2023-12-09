91d7514893352c4d61f8437bf2fb17eb95cffc68
felipepx <feliperaxz@gmail.com>
2023-12-08 21:18:30 -0300

    adicionando link router para profile

--- src/components/FloatingMenu/index.tsx
+++ src/components/FloatingMenu/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Container, Img, Infos, SignOutInfo, StyledFloatingMenu } from './styled';
 import { SignOut } from 'phosphor-react';
+import { Link } from 'react-router-dom';
 
 const FloatingMenu: React.FC = () => {
   return (
     <StyledFloatingMenu>
-      <Img src='https://i.pinimg.com/564x/85/71/72/8571727e081f426167ac04b819ce8dbf.jpg'></Img>
+
+      <Link to="/admin/profile"><Img src='https://i.pinimg.com/564x/85/71/72/8571727e081f426167ac04b819ce8dbf.jpg'></Img></Link>
       <Container>
         <Infos>
           <p>Neymar JR</p>
